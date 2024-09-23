02aa3cab3323486821cb35ed1cc7fc720e054016
David Freixes <dfis1996@gmail.com>
2024-09-23 22:34:05 +0200

    remove constants

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,9 @@
-import localFont from "next/font/local";
 import Header from "./components/Header";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div>
       <Header />
-      
     </div>
   );
 }
